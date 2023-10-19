7b38601799ed8e3c1a34b4057a091fb53bb12531
Mehrshad Cheshm Khavari <ytmehrshad@gmail.com>
2023-10-19 13:47:35 +0330

    Initial commit to challenge no.32

---
+++ explanation.md
@@ -0,0 +1,17 @@
+# Title: "Lucky Number Finder"
+
+# Challenge Description:
+
+In this challenge, you need to write a function called `findLuckyNumber` that accepts an array of numbers (numbers) as input. The function iterates over each number in the array and checks if the number, when converted to a string, contains the digit 7. If a lucky number is found, it is immediately returned. If no lucky number is found, the function returns -1.
+
+## Difficulty: Easy to Medium
+
+## Tests:
+
+```
+input: [1, 2, 3, 4, 5, 6, 7, 8, 9] => output: 7
+
+input: [10, 20, 30, 40, 50] => output: -1
+
+input: [7, 77, 777, 7777] => output: 7
+```
---
+++ tests/challenge.test.js
@@ -0,0 +1,28 @@
+// challenge.test.js
+const fs = require("fs");
+const path = require("path");
+const myFunc = require("../js_challenges");
+
+describe("Multiplication function", () => {
+  // Get the list of JavaScript files in the answers folder
+  const answersFolder = path.join(__dirname, "../answers");
+  const answerFiles = fs.readdirSync(answersFolder);
+
+  // Define the test cases
+  const testCases = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [10, 20, 30, 40, 50],
+    [7, 77, 777, 7777],
+  ];
+
+  // Iterate over each JavaScript file and run the tests
+  answerFiles.forEach((file) => {
+    const answer = require(path.join(answersFolder, file));
+
+    testCases.forEach((input, index) => {
+      test(`Test case ${index + 1} in ${file}`, () => {
+        expect(answer(input)).toEqual(myFunc(input));
+      });
+    });
+  });
+});
