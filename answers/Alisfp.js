bddc3fc57ca00344f56b1461e50da4b10baa4291
Ali koolivand <146428897+Ali-sfpIn@users.noreply.github.com>
2023-10-24 19:56:33 +0330

    Create Alisfp.js

---
+++ answers/Alisfp.js
@@ -0,0 +1,6 @@
+const luckyNumber = function (arr) {
+  if (arr.find(el => String(el).includes(7)))
+    return arr.find(el => String(el).includes(7));
+  else return -1;
+};
+module.exports = luckyNumber
