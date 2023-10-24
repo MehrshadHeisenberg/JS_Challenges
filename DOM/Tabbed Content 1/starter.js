f35aecf7e4ca748944b474cd2d8fa03b05eb4c4f
Mehrshad Cheshm Khavari <127620974+MCKH03@users.noreply.github.com>
2023-10-24 14:34:39 +0330

    Add files via upload

---
+++ DOM/Tabbed Content 1/starter.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const tabContainer = document.querySelector(".tabs");
+const allTabs = document.querySelectorAll(".tab");
+const contentBox = document.querySelector(".tab-content");
+
+const tabContents = [
+  {
+    title: "JS_Challenges?!",
+    text: " JS_Challenges => A great Telegram channel to learn and explore JavaScript!",
+    id: 1,
+  },
+  {
+    title: "Where are the challenges?",
+    text: "Inside a big repository called JS_Challenges!",
+    id: 2,
+  },
+  {
+    title: "Don't forget to ...",
+    text: "Don't forget to like, share and follow!",
+    id: 3,
+  },
+];
