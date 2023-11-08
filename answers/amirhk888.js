f4d364958ffda03be01fde5c80c7d648d2af0779
AmirHK888 <amirhkarimi888@gmail.com>
2023-11-08 14:27:02 +0330

    AmirHK888's solution

---
+++ answers/amirhk888.js
@@ -0,0 +1,13 @@
+const A = [1, 2, 3, 4, 5, 6, 7, 8, 3, -20, 333, 20, 15];
+const B = [3, 6, 8, 15];
+
+const customArrayDiff = (a, b) => {
+    b.map((y) => {
+        a = a.filter(x => x != y ? x : null);
+        a.sort((w, z) => w - z);
+    })
+
+    return a;
+}
+
+console.log(customArrayDiff(A, B));
