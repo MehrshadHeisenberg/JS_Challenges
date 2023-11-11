c3167feacb74a721ad9135f19c68be15f00d5758
Amir H K <119494495+AmirHKarimi888@users.noreply.github.com>
2023-11-11 16:38:57 +0330

    AmirHK888's answer

--- answers/amirhk888.js
+++ answers/amirhk888.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const calcTotalPrice = (items, taxRate, discount) => {
+const calculateTotalPrice = (items, taxRate, discount) => {
     let total = items.reduce((total, item) => total + ((item?.quantity) * (item?.price)) * (1 + (parseFloat(taxRate)/100)) * (1 - (parseFloat(discount)/100)), 0);
     total = total.toFixed(1);
     return total;
 }
 
-module.exports = calcTotalPrice;
+module.exports = calculateTotalPrice;
