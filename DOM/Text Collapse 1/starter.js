0ca09953422ed968b8774d2e210a44612d5915e5
Mehrshad Cheshm Khavari <127620974+MCKH03@users.noreply.github.com>
2023-11-15 09:15:10 +0330

    Add files via upload

---
+++ DOM/Text Collapse 1/starter.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const textBox = document.querySelector(".box");
+const allText = document.querySelectorAll(".text");
+const allTextArr = [...allText];
+
+const data = [
+  {
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum necessitatibus accusamus unde veniam maxime distinctio exercitationem pariatur molestiae harum nam dolorum, nesciunt odio hic tempore eaque id possimus numquam dicta?",
+    id: 1,
+  },
+  {
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores vero maiores non saepe quos beatae temporibus quo corporis perferendis ea quod modi, rerum commodi similique ipsam voluptatum cum molestias culpa.",
+    id: 2,
+  },
+  {
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore illo numquam non voluptatibus odio! Quae, voluptates, ex, similique sed laudantium quibusdam modi aperiam nobis nostrum eveniet ratione eaque voluptatum illo.",
+    id: 3,
+  },
+];
