dc3d016b204bab81faa41e16a015afe97a6538be
Adib Moghimi <119744049+Adib27Moghimi@users.noreply.github.com>
2024-01-08 16:02:22 +0330

    Update-02 adibMoghimi.js

--- answers/adibMoghimi.js
+++ answers/adibMoghimi.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const find7 = function (arr) {
+function find7 (arr) {
   for (let i = 0; i < arr.length; i++) {
     if (String(arr[i]).includes("7")) return arr[i];
   }
   return -1;
-};
+}
 
 module.exports = find7;
