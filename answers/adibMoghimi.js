bdba76e5b104d0ec1b2097f6a1b8cdc3dca08704
Adib Moghimi <119744049+Adib27Moghimi@users.noreply.github.com>
2024-01-08 16:20:33 +0330

    Update-03 adibMoghimi.js
    Mehrshad solution - to test

--- answers/adibMoghimi.js
+++ answers/adibMoghimi.js
@@ -1,3 +1,11 @@
 "use strict";
-const findSeven = (arr) => (String(arr).includes("7") ? 7 : -1);
+
+function find7(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if ((arr[i] + "").includes("7")) return arr[i];
+  }
+
+  return -1;
+}
+
 module.exports = find7;
